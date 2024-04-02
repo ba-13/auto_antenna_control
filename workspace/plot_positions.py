d41ba2c2b231744c888f9f3ba6bc8b6b271a9cd5
ba-13 <b.a.13.official@gmail.com>
2024-04-02 14:53:42 +0530

    Add prediction init

--- workspace/plot_positions.py
+++ workspace/plot_positions.py
@@ -17,10 +17,15 @@ split_data = data.split("\n\n")[:-1]
 
 color = cm.rainbow(np.linspace(0, 1, len(split_data)))
 
-targets = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 1000]
+targets = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 1000] 
 fig, ax = plt.subplots(nrows = 3, ncols=len(targets)//3, num=args.file)
 for i, split in enumerate(split_data):
     df = pd.read_csv(StringIO(split), sep=",", header=None)
-    ax[i % 3][i // 3].scatter(df[0].to_numpy(), df[1].to_numpy(), c=[color[i]], marker='.')
+    times = df[0].to_numpy()
+    locations = df[1].to_numpy()
+    speeds = [y - x for x, y in zip(locations, locations[1:])]
+    accelerations = [y - x for x, y in zip(speeds, speeds[1:])]
+    ax[i % 3][i // 3].scatter(times, locations, c=[color[i]], marker='.')
+    # ax[i % 3][i // 3].scatter(times[:-1], speeds, c=[color[i]], marker='.')
     ax[i % 3][i // 3].set_title(targets[i])
 plt.show()
